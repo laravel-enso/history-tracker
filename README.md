d0a1e5ab23e7b10ed70985a385ce2505eb049362
mishu <mishu@xtelecom.ro>
2017-03-19 20:48:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# HistoryTracker
+
+Trait for tracking a table histories
+
+# Don't forget to
+
+use protected static $historyModel = 'tableThatNeedsHistoryTracking'
+
+## Upgrade from laravel-enso v2
+
+Correct all the includes for the classes included in this package
+TrackHistory has been rename to HistoryTracker
+
---
+++ src/Traits/HistoryTracker.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LaravelEnso\HistoryTracker\Traits;
+
+trait HistoryTracker {
+
+    //protected static $historyModel = '';
+
+    protected static function bootTrackHistory()
+    {
+        static::created(function($model)
+        {
+            static::saveHistory($model);
+        });
+
+        static::updated(function($model)  {
+            static::saveHistory($model);
+        });
+    }
+
+    private static function saveHistory($model)
+    {
+        $history = new static::$historyModel;
+
+        //the history model needs  to have both updated_by and created_by
+        //attributes as fillable
+        $history->fill($model->toArray());
+        $model->histories()->save($history);
+    }
+
+    public function histories()
+    {
+        return $this->hasMany(static::$historyModel);
+    }
+}
