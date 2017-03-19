01c51ee8f4e429080f4c04dfeab45a7d5aa16105
mishu <mishu@xtelecom.ro>
2017-03-19 20:58:31 +0200

    fix unused variable

--- src/Traits/HistoryTracker.php
+++ src/Traits/HistoryTracker.php
@@ -4,22 +4,22 @@ namespace LaravelEnso\HistoryTracker\Traits;
 
 trait HistoryTracker
 {
-    private static $historyModel = '';
+    protected static $historyModel = '';
 
     protected static function bootTrackHistory()
     {
-        static::created(function ($model) {
-            static::saveHistory($model);
+        self::created(function ($model) {
+            self::saveHistory($model);
         });
 
-        static::updated(function ($model) {
-            static::saveHistory($model);
+        self::updated(function ($model) {
+            self::saveHistory($model);
         });
     }
 
     private static function saveHistory($model)
     {
-        $history = new static::$historyModel();
+        $history = new $this->historyModel;
 
         //the history model needs  to have both updated_by and created_by
         //attributes as fillable
@@ -29,6 +29,6 @@ trait HistoryTracker
 
     public function histories()
     {
-        return $this->hasMany(static::$historyModel);
+        return $this->hasMany($this->$historyModel);
     }
 }
