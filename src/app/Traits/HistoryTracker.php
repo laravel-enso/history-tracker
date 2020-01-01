90275959d175bc845bddbe1c9eb22989ed848ba1
Adrian Ocneanu <aocneanu@gmail.com>
2020-01-01 21:36:31 +0000

    Apply fixes from StyleCI

--- src/app/Traits/HistoryTracker.php
+++ src/app/Traits/HistoryTracker.php
@@ -10,9 +10,9 @@ trait HistoryTracker
 
     protected static function bootHistoryTracker()
     {
-        self::created(fn($model) => $model->saveHistory());
+        self::created(fn ($model) => $model->saveHistory());
 
-        self::updated(fn($model) => $model->saveHistory());
+        self::updated(fn ($model) => $model->saveHistory());
 
         self::deleted(function ($model) {
             if (method_exists($model, 'bootSoftDeletes')) {
